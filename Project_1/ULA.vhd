0eee7b02012f5fd4fd6ad638f9b532d342c6b124
Daniel Abreu <daniel.frioli@gmail.com>
2026-04-28 11:34:15 -0300

    Added For Loop in ALU Testbench for all Test Cases

--- Project_1/ULA.vhd
+++ Project_1/ULA.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Definição Comportamental da Unidade Lógica Aritmética:
+-- Definição Comportamental da Unidade Lógica Aritmética (Unsigned):
 
 entity ULA is
     port(
--- Project_1/testbench.vhd
+++ Project_1/testbench.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity Testbench_ULA is
 end Testbench_ULA;
@@ -23,7 +24,13 @@ architecture Behavior of Testbench_ULA is
     signal y_s       : std_logic_vector(7 downto 0);
     signal c_s       : std_logic;
 
+    signal s_a : unsigned(3 downto 0):= "0000";
+    signal s_b : unsigned(3 downto 0):= "0000";
+
 begin
+
+    a_s <= std_logic_vector(s_a);
+    b_s <= std_logic_vector(s_b);
 
     DUT: ULA 
         port map(
@@ -36,44 +43,39 @@ begin
 
     process is
     
+    variable v_a : unsigned(3 downto 0) := "0000";
+    variable v_b : unsigned(3 downto 0) := "0000";
+
     begin
+		
+        -- Tabela de Operações:
+        -- Adição: 			"0000"
+        -- Subtração:		"0001"
+        -- Multiplicação:	"0010"
+        -- Divisão:			"0011"
+        -- ABS(A):			"0100"
+        -- REM:				"0101"
+        -- MOD:				"0110"
+        -- AND:				"0111"
+        -- OR:				"1000"
+        -- NOT(A):			"1001"
+        -- NAND:			"1010"
+        -- NOR:				"1011"
+        -- XOR:				"1100"
+        -- XNOR:			"1101"
+        
+        op_s <= "0000"; -- Mudar Opção do Seletor de Acordo Com a Operação.
 
-        -- ADD
-        a_s <= "0011"; b_s <= "0010"; op_s <= "0000";
-        wait for 10 ns;
+        for i in 0 to 15 loop
+            s_a <= v_a;
+            v_a := v_a + 1;
 
-        -- SUB
-        op_s <= "0001";
-        wait for 10 ns;
-
-        -- MUL
-        op_s <= "0010";
-        wait for 10 ns;
-
-        -- DIV
-        op_s <= "0011";
-        wait for 10 ns;
-
-        -- ABS
-        a_s <= "1101"; op_s <= "0100"; -- -3
-        wait for 10 ns;
-
-        -- REM
-        a_s <= "0111"; b_s <= "0011"; op_s <= "0101";
-        wait for 10 ns;
-
-        -- MOD
-        op_s <= "0110";
-        wait for 10 ns;
-
-        -- LOGICAL TESTS
-        op_s <= "0111"; wait for 10 ns; -- AND
-        op_s <= "1000"; wait for 10 ns; -- OR
-        op_s <= "1001"; wait for 10 ns; -- NOT
-        op_s <= "1010"; wait for 10 ns; -- NAND
-        op_s <= "1011"; wait for 10 ns; -- NOR
-        op_s <= "1100"; wait for 10 ns; -- XOR
-        op_s <= "1101"; wait for 10 ns; -- XNOR
+            for j in 0 to 15 loop
+                s_b <= v_b;
+                v_b := v_b + 1;
+                wait for 1 ns; -- Mínimo de (16 * 16) = 256 ns. 
+            end loop;
+        end loop;
 
     end process;
 
